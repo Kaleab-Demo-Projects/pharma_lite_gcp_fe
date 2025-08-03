b21349b9d293d9d7ccc53d5b44c13da15776755c
Kaleab Ero <kaleabero@Mac.attlocal.net>
2025-08-03 13:29:14 -0500

    add some content to order dashboard and medicines page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { App } from './app';
 
 export const routes: Routes = [
-  { path: '', component: App },
-  { path: 'dashboard', loadComponent: () => import('./features/dashboard').then(m => m.Dashboard) },
-  { path: 'medicines', loadComponent: () => import('./features/medicines').then(m => m.Medicines) },
-  { path: 'orders', loadComponent: () => import('./features/orders').then(m => m.Orders) },
+  {
+    path: '',
+    component: App,
+    children: [
+      { path: '', loadComponent: () => import('./features/home/home').then(m => m.Home) },
+      { path: 'dashboard', loadComponent: () => import('./features/dashboard').then(m => m.Dashboard) },
+      { path: 'medicines', loadComponent: () => import('./features/medicines').then(m => m.Medicines) },
+      { path: 'orders', loadComponent: () => import('./features/orders').then(m => m.Orders) },
+    ],
+  }
 ];
