a507ac4c936e0d8639f5d49ed911faa73d7ddb5f
Kaleab Ero <kaleabero@Mac.attlocal.net>
2025-08-02 22:03:22 -0500

    intial code commit

---
+++ src/app/features/medicines/medicines.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicines',
+  imports: [],
+  templateUrl: './medicines.html',
+  styleUrl: './medicines.scss'
+})
+export class Medicines {
+
+}
