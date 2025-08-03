9d74b80e2d38dd2eefb4d2610ef7be4e64f07658
Kaleab Ero <kaleabero@Mac.attlocal.net>
2025-08-02 22:07:36 -0500

    initial commits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: App },
+  { path: 'dashboard', loadComponent: () => import('./features/dashboard').then(m => m.Dashboard) },
+  { path: 'medicines', loadComponent: () => import('./features/medicines').then(m => m.Medicines) },
+  { path: 'orders', loadComponent: () => import('./features/orders').then(m => m.Orders) },
+];
